189db4e9b6caffc79fbf402e47ee23c3bdfa4f4c
eric2969 <eric2969@gmail.com>
2024-12-06 16:17:23 +0800

    member_login

--- backend/member_login.php
+++ backend/member_login.php
@@ -47,9 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($input['username']) && isset($
 
     if ($stmt->num_rows > 0 && password_verify($password, $hashed_password)) {
         $_SESSION['member'] = $username;
-        if ($remember) {
-            setcookie('member', $username, time() + (3000), "/"); // 5min
-        }
+        setcookie('member', $username, time() + (3000), "/"); // 5min
         echo json_encode(['success' => true, 'message' => '登入成功']);
     } else {
         echo json_encode(['success' => false, 'message' => '用戶名或密碼錯誤']);
