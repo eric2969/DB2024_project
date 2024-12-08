b21ad53597a2ac4875a956aaf8d5338443565768
XDturtleXD <turtle3491318@gmail.com>
2024-12-09 00:07:10 +0800

    Added get_order.js and mem_order.php

--- backend/mem/remember.php
+++ backend/mem/remember.php
@@ -1,13 +1,15 @@
 <?php
 session_start();
+function remember(){
+    if (!isset($_SESSION['member']) && isset($_COOKIE['member'])) {
+        $_SESSION['member'] = $_COOKIE['member'];
+    }
 
-if (!isset($_SESSION['admin']) && isset($_COOKIE['admin'])) {
-    $_SESSION['admin'] = $_COOKIE['admin'];
+    if (isset($_SESSION['admin'])) {
+        echo json_encode(['logged_in' => true, 'username' => $_SESSION['member']]);
+    } else {
+        echo json_encode(['logged_in' => false]);
+    }
 }
 
-if (isset($_SESSION['admin'])) {
-    echo json_encode(['logged_in' => true, 'username' => $_SESSION['admin']]);
-} else {
-    echo json_encode(['logged_in' => false]);
-}
 ?>
